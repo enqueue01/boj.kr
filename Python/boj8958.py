307cf5c022f490f69e5bc57d4b570800f5e01350
song-ingyu <songik@kakao.com>
2023-10-11 10:08:43 +0900

    일괄 커밋 및 푸시

--- Python/boj8958.py
+++ Python/boj8958.py
@@ -1,7 +1,12 @@
-# N = int(input())
-# for i in range(N):
-#     score = input().split("X")
-
-s="OXOOXXOXOOO"
-print(s.index("X"))
-print(s)
+N = int(input())
+result, ac = 0, 1
+for i in range(N):
+    score = input()
+    for j in score:
+        if j == "O":
+            result = result + ac
+            ac = ac + 1
+        else:
+            ac = 1
+    print(result)
+    result, ac = 0, 1
